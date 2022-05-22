d6e12b2919eb61ee02451b77ec1658dd6b7b484e
Hongleang Lim <73740288+hongleang@users.noreply.github.com>
2022-05-22 11:40:06 +1000

    Setup sanity schema and components

---
+++ ecommerce/components/index.js
@@ -0,0 +1,15 @@
+import Cart from './Cart';
+import Footer from './Footer';
+import FooterBanner from './FooterBanner';
+import HeroBanner from './HeroBanner';
+import Navbar from './Navbar'; 
+import Product from './Product';
+
+export {
+    Cart,
+    Footer,
+    FooterBanner,
+    HeroBanner,
+    Navbar,
+    Product
+}
